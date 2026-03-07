7c4af6852402a03e2a470d01d51bf94de4ce6c02
Ilan Laloum <isramail@gmail.com>
2026-03-07 13:32:12 +0200

    Update version to 1.2.2 and format code in SpreadsheetApp class

--- src/spreadsheetapp/SpreadsheetApp.js
+++ src/spreadsheetapp/SpreadsheetApp.js
@@ -1,9 +1,24 @@
 const Spreadsheet = require('./classes/Spreadsheet')
 
 class SpreadSheetApp {
-  static getActiveSpreadsheet () {
-    return Spreadsheet
+  static getActiveSpreadsheet() {
+    return Spreadsheet;
+  }
+
+  static getUi() {
+    // createAddonMenu
+    return {
+      createAddonMenu: function () {
+        return this;
+      },
+      addItem: function (name, functionName) {
+        return this;
+      },
+      addToUi: function () {
+        return this;
+      }
+    }
   }
 }
 
-module.exports = SpreadSheetApp
+module.exports = SpreadSheetApp;
